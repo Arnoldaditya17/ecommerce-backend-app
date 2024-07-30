3e59f90714f49f56fd957e27f5dca06fb98ad66d
Arnoldaditya17 <adityasinghrajput@sistec.ac.in>
2024-07-30 23:18:59 +0530

    first commit

---
+++ src/main/java/com/aditya/ecommerce_backend_app/EcommerceBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.ecommerce_backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/ecommerce_backend_app/EcommerceBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.ecommerce_backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

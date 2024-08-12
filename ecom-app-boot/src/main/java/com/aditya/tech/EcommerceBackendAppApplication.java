e2f908c6549a5ea9ff5f8437cd387b3df448e6b2
Arnoldaditya17 <adityasinghrajput@sistec.ac.in>
2024-08-12 15:31:57 +0530

    module setup

---
+++ ecom-app-boot/src/main/java/com/aditya/tech/EcommerceBackendAppApplication.java
@@ -0,0 +1,15 @@
+package com.aditya.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class EcommerceBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendAppApplication.class, args);
+	}
+
+}

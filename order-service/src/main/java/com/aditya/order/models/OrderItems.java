8562cbcd6d89a1e2ba691a446610da0d15227178
Arnoldaditya17 <adityasinghrajput@sistec.ac.in>
2024-08-27 17:11:39 +0530

    basic model created

--- order-service/src/main/java/com/aditya/order/models/OrderItems.java
+++ order-service/src/main/java/com/aditya/order/models/OrderItems.java
@@ -1,5 +1,36 @@
 package com.aditya.order.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
 public class OrderItems {
+
+
+    @Id
+    private String id;
+
+    private String productId;
+
+    private String productSku;
+
+    private int quantity;
+
+    private int mrp;
+
+    private  int salePrice;
+
+    private Date orderDate;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+
 
 }
